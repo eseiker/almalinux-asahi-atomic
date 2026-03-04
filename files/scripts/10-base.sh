81be0a76fdc80fad868021d0326bb3159236ffe9
Licia <git@licia.dev>
2026-03-04 20:10:05 +0900

    update 10-base.sh

--- files/scripts/10-base.sh
+++ files/scripts/10-base.sh
@@ -2,12 +2,16 @@
 
 set -xeuo pipefail
 
-dnf install -y dnf-plugins-core epel-release
+dnf -y install dnf-plugins-core epel-release
 dnf config-manager --set-enabled crb
 dnf -y update
-dnf -y copr enable eseiker/asahi-el-kernel
+
 ostree config set sysroot.bootprefix true
+dnf -y copr enable eseiker/asahi-el-kernel
 dnf -y swap kernel* kernel-16k --nogpgcheck
+
 # dracut generate initramfs workaround
 kver=$(cd /usr/lib/modules && echo *); \
   dracut -vf /usr/lib/modules/$kver/initramfs.img $kver
+
+dnf -y install system-reinstall-bootc
