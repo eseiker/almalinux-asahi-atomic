7985022287cad38a9c10c0085bb49d00229eb284
Licia Seiker <git@licia.dev>
2026-03-09 17:33:56 +0900

    rebuilt kernel

--- files/scripts/10-base.sh
+++ files/scripts/10-base.sh
@@ -8,11 +8,10 @@ dnf -y update
 
 ostree config set sysroot.bootprefix true
 dnf -y copr enable eseiker/asahi-el-kernel
-dnf -y --nogpgcheck swap kernel* kernel-16k
-dnf -y --nogpgcheck install kernel-16k-modules-extra systemd-boot-unsigned
+dnf -y swap kernel* kernel-16k
+dnf -y --setopt=install_weak_deps=False install \
+  kernel-16k-modules-extra systemd-boot-unsigned system-reinstall-bootc @core
 
 # dracut generate initramfs workaround
 kver=$(cd /usr/lib/modules && echo *); \
   dracut --no-hostonly --reproducible --show-modules --uefi -vf /usr/lib/modules/$kver/initramfs.img $kver
-
-dnf -y --nogpgcheck --setopt=install_weak_deps=False install system-reinstall-bootc @core
