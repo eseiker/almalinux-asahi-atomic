5f1348ff07e664e3617459d409c52da1abcc7893
Licia Seiker <git@licia.dev>
2026-03-09 17:51:37 +0900

    iso

--- files/scripts/10-base.sh
+++ files/scripts/10-base.sh
@@ -2,15 +2,13 @@
 
 set -xeuo pipefail
 
-dnf -y install dnf-plugins-core epel-release
-dnf config-manager --set-enabled crb
-dnf -y update
-
 ostree config set sysroot.bootprefix true
+
+dnf -y install dnf-plugins-core
 dnf -y copr enable eseiker/asahi-el-kernel
 dnf -y swap kernel* kernel-16k
-dnf -y --setopt=install_weak_deps=False install \
-  kernel-16k-modules-extra systemd-boot-unsigned system-reinstall-bootc @core
+dnf -y install kernel-16k-modules-extra systemd-boot-unsigned system-reinstall-bootc @core
+dnf -y update
 
 # dracut generate initramfs workaround
 kver=$(cd /usr/lib/modules && echo *); \
